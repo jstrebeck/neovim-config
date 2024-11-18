5a0fdc36986f6b532a8eae6ede67905d0cd1a372
Josh Strebeck <jstrebeck@paytrace.com>
2024-11-18 10:22:44 -0800

    Add base plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,34 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+      "kdheepak/lazygit.nvim",
+      lazy = true,
+      cmd = {
+          "LazyGit",
+          "LazyGitConfig",
+          "LazyGitCurrentFile",
+          "LazyGitFilter",
+          "LazyGitFilterCurrentFile",
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+      -- setting the keybinding for LazyGit with 'keys' is recommended in
+      -- order to load the plugin when the command is run for the first time
+      keys = {
+          { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      }
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  		 "vim", "lua", "vimdoc",
+       "html", "css" , "hcl",
+       "terraform",
+  		},
+  	},
+  },
 }
