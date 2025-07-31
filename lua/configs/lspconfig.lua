a1d99b817364798df9f074091043ae3154667ca8
jstrebeck <jstrebeck@paytrace.com>
2025-07-31 10:56:55 -0700

    Add terraform LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "terraformls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,6 +15,20 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+require'lspconfig'.terraformls.setup{
+  on_attach = nvlsp.on_attach, -- Keep the NVChad default on_attach
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    terraformls = {
+      -- This is the crucial part where you define the code action on save
+      lsp_code_actions_on_save = {
+        ["source.formatAll.terraform"] = true,
+      },
+    },
+  },
+}
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
